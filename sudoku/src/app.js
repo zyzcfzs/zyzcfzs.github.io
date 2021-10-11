1cda18fe21eee810b7341eee91c46d5933a03a46
zyzcfzs <zyzcfzs@163.com>
2021-10-12 00:31:42 +0800

    last change

---
+++ sudoku/src/app.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./css/style.css"
+import Solver from "./solver.js"
+const App = () => {
+    return (
+        <div>
+         <h1>Sudoku Solver</h1>
+        <Solver></Solver>
+        </div>
+    )
+}
+
+export default App

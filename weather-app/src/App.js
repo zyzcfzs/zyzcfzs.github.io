b9f3e9920f98eeba1234f4e249536cf8c57f2d64
zyzcfzs <zyzcfzs@163.com>
2021-10-12 00:22:13 +0800

    last change

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,13 @@
-import Title from "./Title";
-
-function App() {
-  return (
-    <div className="App">
-      <Title></Title>
-    </div>
-  );
+import React from 'react'
+import "./css/style.css"
+import Solver from "./solver.js"
+const App = () => {
+    return (
+        <div>
+         <h1>Sudoku Solver</h1>
+        <Solver></Solver>
+        </div>
+    )
 }
 
-export default App;
+export default App
